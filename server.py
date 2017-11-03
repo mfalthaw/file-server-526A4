c8de68b72c8feaa03ec6e0d1f421791282408c69
Moath <mfalthaw@ucalgary.ca>
2017-11-03 14:45:45 -0600

    communication handlers

--- server.py
+++ server.py
@@ -14,6 +14,31 @@ HOST = '127.0.0.1'
 PORT = 8000
 
 '''
+Handles sending messages to client
+'''
+def sendMsg(sock, str):
+	sock.send(str.encode('utf-8'))
+
+'''
+Handles sending data to client
+'''
+def sendData(sock, data):
+	sock.send(data)
+
+'''
+Handles receiving data from client
+'''
+def recvData(sock):
+	return sock.recv(BUFFER_SIZE)
+
+'''
+Handle new client
+'''
+def handleClient(name, sock):
+	# sock.
+	print("handleClient")
+
+'''
 Handles receiving files from client
 '''
 def receiveFile(name, sock):
@@ -24,33 +49,34 @@ Handles sending files to client
 '''
 def sendFile(name, sock):
 	while True:
-		fileName = sock.recv(BUFFER_SIZE)
+		fileName = recvData(sock)
 		fileName = fileName.decode()
 
 		print('File Name received: {}'.format(fileName))
 		if os.path.isfile(fileName):
 			print('Found File')
 			fileSize = os.path.getsize(fileName)
-			sock.send(('Success! File Size: ' + str(fileSize)).encode('utf-8'))
+			sendMsg(sock, 'Success! File Size: ' + str(fileSize))
 			
-			userResponse = sock.recv(BUFFER_SIZE)
+			userResponse = recvData(sock)
 			userResponse = userResponse.decode()
 			
 			if userResponse.lower().startswith('ok'):
 				# start sending file
 				with open(fileName, 'rb') as file:
 					bytesToSend = file.read(BUFFER_SIZE)
-					sock.send(bytesToSend)
+					sendData(sock, bytesToSend)
 					
-					while bytesToSend != '':
+					while bytesToSend.decode() != '':
 						bytesToSend = file.read(BUFFER_SIZE)
-						sock.send(bytesToSend)
-						bytesToSend = bytesToSend.decode()
+						sendData(sock, bytesToSend)
+
 					print('file transfer completed!')
 			else:
 				print('userResponse was: {}'.format(userResponse))
 		else:
-			sock.send(("Fail! Can't find: {}".format(fileName)).encode('utf-8'))
+			# sock.send(("Fail! Can't find: {}".format(fileName)).encode('utf-8'))
+			send(sock, "Fail! Can't find: {}".format(fileName))
 
 	# close connection
 	sock.close()
